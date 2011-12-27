c862eede82b3ff36744ca482615b9dc5a59ee97f
Mauricio Ruiz <maudiruiz@gmail.com>
2011-12-27 13:00:40 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quickyfit::Application.config.secret_token = '957ab123bd48c36955b3c712a4c8c0ca42b07e10bb2833411a44cbb24fa9c796663e1b9d9030c0bd3a5cbb744e6d722caefa05a81d41a0833bfcd4de2bc28cb4'
