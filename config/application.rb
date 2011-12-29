9ebf85e3ed4fec1daced7e02cbe1f0afdc98e8eb
Mauricio Ruiz <maudiruiz@gmail.com>
2011-12-28 19:15:34 -0600

    appp

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Quickyfit
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.precompile << "locales/*.js"
+    
+    config.assets.precompile += %w(rails_admin/rails_admin.js rails_admin/rails_admin.css)
   end
 end
