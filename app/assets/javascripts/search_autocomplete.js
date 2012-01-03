dcd12402e55b2dcc40ecc691a92747f47de5f5c4
Mauricio Ruiz <maudiruiz@hotmail.com>
2012-01-02 19:12:36 -0600

    ok

--- app/assets/javascripts/search_autocomplete.js
+++ app/assets/javascripts/search_autocomplete.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $("input.drug_autocomplete").autocomplete({
+  $("#quiero").autocomplete({
     source: '/display/get_drugs',
       // focus: function(event, ui) {
       //   $('input.drug_autocomplete').val(ui.item.product_name);
@@ -10,6 +10,7 @@ $(document).ready(function(){
     select: function(event, ui) {
   	console.log("Ok");
 $(this).val(ui.item.description);
+$("#number1").val(ui.item.id);
 console.log(this);
      return false;
     }
@@ -23,6 +24,31 @@ console.log(this);
       return $( "<li></li>" ).data( "item.autocomplete", item )
       .append( "<a>" + display_name + "</a>" ).appendTo( ul );
    };
+  $("#tengo").autocomplete({
+    source: '/display/get_drugs',
+      // focus: function(event, ui) {
+      //   $('input.drug_autocomplete').val(ui.item.product_name);
+      //   return false;
+      // },
+
+    select: function(event, ui) {
+  	console.log("Ok");
+$(this).val(ui.item.description);
+$("#number2").val(ui.item.id);
+console.log(this);
+     return false;
+    }
+
+  })
+
+   .data( "autocomplete" )._renderItem = function( ul, item ) {
+	//return item.description;
+	console.log(item);
+   var display_name = item.description;
+      return $( "<li></li>" ).data( "item.autocomplete", item )
+      .append( "<a>" + display_name + "</a>" ).appendTo( ul );
+   };
 
 	
 });
+
