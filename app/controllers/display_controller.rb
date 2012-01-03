9c806f4c6c8cf5ad8a8d7c34c78223859fa3244a
Benjamin Hernandez <benjamin.hernandez@in2teck.com>
2012-01-03 12:15:30 -0600

    Se hicieron mejoras a las busquedas

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -1,17 +1,30 @@
 class DisplayController < ApplicationController
+  
   def index
-    @items = Item.all
-    @contributions = Contribution.all
-    @number1 = params[:number1]
-    @number2 = params[:number2]
-    @itemsok = Contribution.where("first_item_id = ?",@number1).where("second_item_id = ?",@number2)
+    
+    if (not params[:number1].nil?) and (params[:number1] != "")
+      item1 = Item.find(params[:number1])
+    elsif not params[:quiero].nil?
+      item1 = Item.find(:first, :conditions => ['lower(description) LIKE ?', "%#{params[:quiero].downcase}%"])
+    end
+
+    if (not params[:number2].nil?) and (params[:number2] != "")
+      item2 = Item.find(params[:number2])
+    elsif not params[:tengo].nil?
+      item2 = Item.find(:first, :conditions => ['lower(description) LIKE ?', "%#{params[:tengo].downcase}%"])
+    end
+
+    if (item1 and item2)
+      @itemsok = Contribution.where("first_item_id = ?",item1.id).where("second_item_id = ?",item2.id)
+    end
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @contributions }
     end
   end
+  
   def get_drugs
-    
     render :json => Item.search(params[:term])
   end
 end
