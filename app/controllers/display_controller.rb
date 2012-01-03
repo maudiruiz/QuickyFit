b53a46fc3e80256a0e114ed11d7d45572f43dd57
Mauricio Ruiz <maudiruiz@gmail.com>
2012-01-03 08:39:51 -0600

    corregi el enviador

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -4,7 +4,7 @@ class DisplayController < ApplicationController
     @contributions = Contribution.all
     @number1 = params[:number1]
     @number2 = params[:number2]
-    @itemsok = Contribution.where("first_item_id = ?",@number1).where("first_item_grade = ?",@number2)
+    @itemsok = Contribution.where("first_item_id = ?",@number1).where("second_item_id = ?",@number2)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @contributions }
